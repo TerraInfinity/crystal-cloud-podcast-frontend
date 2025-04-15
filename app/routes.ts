b91cdc3ea5f3a99585e1eae1b16df006ebadcbbc
TerraInfinity <career@terrainfinity.ca>
2025-04-15 01:34:32 -0700

    routes.ts updated

--- app/routes.ts
+++ app/routes.ts
@@ -5,8 +5,8 @@ export default [
   index("routes/landing.tsx"),       // renders at /
   route("home", "routes/home.tsx"),  // Rendered at /home
   route("login", "routes/login.tsx"),  // Rendered at /login
-  route("blogDetail", "routes/blogDetail.tsx"),  // Rendered at /blogDetails
-  route("adminDashboard", "routes/adminDashboard.tsx"),  // Rendered at /adminDashboard
+  route("blog/:id", "routes/blogDetail.tsx"),  // Rendered at /blog/:id (e.g., /blog/043ae60e-312e-4a99-be29-1874b9324be4)
+  route("admin", "routes/adminDashboard.tsx"),  // Rendered at /adminDashboard
   route("restrictedAccess", "pages/errors/RestrictedAccess.tsx"),  // Rendered at /403
   route("serverError", "pages/errors/ServerError.tsx"),  // Rendered at /500
   route("unauthorizedAccess", "pages/errors/UnauthorizedAccess.tsx"),  // Rendered at /401
